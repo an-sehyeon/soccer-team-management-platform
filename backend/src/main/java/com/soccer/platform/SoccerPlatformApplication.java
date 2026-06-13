0bc0fc67212ef8f4bbdbcb718bbf266418d4ea77
세현 <nocount334@gmail.com>
2026-06-14 03:58:57 +0900

    spring boot commit

---
+++ backend/src/main/java/com/soccer/platform/SoccerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerPlatformApplication.class, args);
+	}
+
+}
