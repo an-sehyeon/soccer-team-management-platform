44ec140da5ebe5adcbd2b2d364f55c552e985668
세현 <nocount334@gmail.com>
2026-06-29 21:36:49 +0900

    경기 영상 정적 파일 재생을 위한 업로드 경로 접근 허용

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,19 +1,13 @@
-// 백엔드 API 요청에 공통 설정과 Access Token을 적용하는 Axios 인스턴스 파일
+// 공통 axios 인스턴스와 인증 헤더 처리를 관리하는 파일
 
 import axios from "axios";
 import { getAccessToken, removeAccessToken } from "../utils/tokenStorage";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-// 백엔드 API 공통 Axios 인스턴스
 export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// 요청마다 Access Token을 Authorization 헤더에 추가
+// 요청마다 Access Token이 있으면 Authorization 헤더 추가
 axiosInstance.interceptors.request.use((config) => {
   const accessToken = getAccessToken();
 
@@ -24,7 +18,7 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-// 인증 실패 응답을 받으면 저장된 토큰 삭제
+// 인증 실패 시 Access Token 제거
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
